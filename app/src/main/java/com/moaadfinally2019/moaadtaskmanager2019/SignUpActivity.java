6b0992e02f2a5e5222e61c0d9edd2f6168bf7f4c
user <nana-aslan@hotmail.com>
2018-10-16 13:21:44 +0300

    sign in/Up and fireBase

--- app/src/main/java/com/moaadfinally2019/moaadtaskmanager2019/SignUpActivity.java
+++ app/src/main/java/com/moaadfinally2019/moaadtaskmanager2019/SignUpActivity.java
@@ -8,7 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth auth;// to make SignIn ,SignUp
+    private FirebaseUser user;//user
     private TextView tvFirst;
     private TextView tvLast;
     private EditText etPssword;
@@ -18,7 +23,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();//
         etEmail = (EditText)findViewById(R.id.etEmail);
         etPssword = (EditText)findViewById(R.id.etPassword);
         btnSave = (Button) findViewById(R.id.btnSave);
