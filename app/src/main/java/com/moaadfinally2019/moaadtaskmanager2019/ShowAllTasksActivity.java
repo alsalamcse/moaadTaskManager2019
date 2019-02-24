9e8c31bef16428605ed91f23b13b14bc6df99164
moaad <rakaz.hammad12@gmail.com>
2019-02-24 11:34:14 +0200

    adapter

---
+++ app/src/main/java/com/moaadfinally2019/moaadtaskmanager2019/ShowAllTasksActivity.java
@@ -0,0 +1,16 @@
+package com.moaadfinally2019.moaadtaskmanager2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ShowAllTasksActivity extends AppCompatActivity {
+    ListView lvTasks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_tasks);
+        lvTasks=findViewById()
+    }
+}
